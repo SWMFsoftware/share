d6c2a691a74f45e2669e890f6b4fe8060b38497c
Zhenguang Huang <zghuang@umich.edu>
2020-08-04 22:36:47 -0400

    Added the script, which is quite similar as setenv_insitu.sh. This script would call compare_insitu in sswidl.

---
+++ IDL/Solar/compare_insitu.sh
@@ -0,0 +1,18 @@
+#!/bin/tcsh
+
+setenv IDL_PATH +$1/share/IDL
+setenv SSW $HOME/ssw
+setenv SECCHI $SSW/stereo/secchi
+setenv SECCHI_CAL $SECCHI/calibration
+setenv NRL_LIB $SSW/soho/lasco
+setenv SSW_INSTR "gen soho aia hmi xrt eit lasco secchi nrl festival"
+source $SSW/gen/setup/setup.ssw
+setenv SSWDB $HOME/sswdb
+setenv SECCHI_BKG $SSWDB/stereo/secchi/backgrounds
+setenv MONTHLY_IMAGES $SSWDB/soho/lasco/monthly
+setenv CDF_LEAPSECONDSTABLE CDFLeapSeconds
+setenv IDL_STARTUP ssw_startup_insitu
+
+printf "compare_insitu, dir_sim='$2', dir_plot='$3/'" | sswidl > $3/log_insitu
+
+exit
