490dd71539881625039ed7b430e9d70c17338cf8
Gabor Toth <gtoth@umich.edu>
2021-04-07 21:59:46 -0400

    Previos code was not right because ls -Rt does not sort all the files in all subdirectories. Turns out that
    find [PATH] -newer FILE 2>&1
    
    does exactly what we need. The redirection is useful if FILE does not exist.
    But why not use a Perl script? :-) So
    
    uptodate.pl FILE [PATH]
    
    does the above, and if PATH is not present, it is replaced with . (find does the same).

--- Scripts/uptodate.pl
+++ Scripts/uptodate.pl
@@ -1,23 +1,20 @@
 #!/usr/bin/perl -s
-# Usage: uptodate.pl [-v] FILE DIR
-# returns empty string if file is newer than any file in directory DIR
-# returns "UPDATE" otherwise
-# -v is for verbose output.
-my $verbose = $v;
+# Usage: uptodate.pl [-v] FILE [FILE1 FILE2 DIR1 DIR2 ...]
+# Returns "FILE does not exist" if FILE does not exist.
+# Returns all files that are newer than FILE listed
+# in the optional file and directory arguments.
+# If there is no optional argument, the local directory is used.
+# If there is nothing newer and FILE exists, an empty string is returned.
+# This can be used in Makefile-s as
+# if [ `uptodate.pl ${TARGETFILE} $DEPENDENCIES` ]; then ... ; fi
 
 use strict;
 
-my $file = $ARGV[0] or die "ERROR in uptodate.pl: Missing file name\n";
-my $dir  = ($ARGV[1] or ".");
-print "file=$file dir=$dir\n" if $verbose;
+my $file = shift @ARGV or die "ERROR in uptodate.pl: Missing file name\n";
+my $dir  = (join(" ", @ARGV) or ".");
 
 if(-f $file){
-    my $filetime = -M $file;
-    my $latest = `ls -tR $dir | head -1`; chop $latest;
-    my $latesttime = -M $latest;
-    print "$file modtime $filetime, $latest modtime=$latesttime: ",
-	$filetime < $latesttime,"\n" if $verbose;
-    if( $filetime <= $latesttime){exit};
+    print `find $dir -newer $file`; 
+}else{
+    print "$file does not exist\n";
 }
-print "UPDATE\n";
-
