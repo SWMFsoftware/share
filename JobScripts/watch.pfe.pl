19e08bfdaa9406363b3255056b9ae7ca7bec0623
Gabor Toth <gtoth@umich.edu>
2021-12-23 15:57:42 -0500

    Make sure that the qdel really deletes the jobs. Avoids multiple jobs running in parallel after watch.pfe.pl finishes.

--- JobScripts/watch.pfe.pl
+++ JobScripts/watch.pfe.pl
@@ -2,6 +2,8 @@
 use strict;
 
 my $pattern = $ARGV[0];
+my $qstat   = '/PBS/bin/qstat -u $USER';
+my $qdel    = '/PBS/bin/qdel';
 
 if(not $pattern){
     print "
@@ -10,7 +12,7 @@ Usage: watch.pfe.pl PATTERN >& watch.log &
 Watch jobs matching pattern. If any of them starts to run, delete the others.
 The following jobs are available now:
 
-",`qstat -u \$USER`,"
+",`$qstat`,"
 
 Use PATTERN to select a subset of these, e.g.
 
@@ -23,27 +25,39 @@ my @results;
 my $running;
 my $user = $ENV{USER};
 LOOP:{
-    @results = `/PBS/bin/qstat -u \$USER | grep $pattern`;
+    @results = `$qstat | grep $pattern`;
     my $ids;
     foreach (@results){
-	#/^(\d+)[^:]+:\d\d ([A-Z]) +((\dd\+)?\d\d:\d\d)/;
-	/^(\d+).*([A-Z]) +(\S+)/;
-	print "id=$1 status=$2 wait=$3\n";
+	/^(\d+).*([A-Z]) +\S+/;
+	print "id=$1 status=$2: $_";
 	$ids .= " $1";
 	$running = $1 if $2 eq "R";
     }
     print "-------------------------\n";
     if($running){
-	$ids =~ s/ $running//;
-	print "qdel $ids\n";
-	`/PBS/bin/qdel $ids`;
+	$ids =~ s/ $running//; # remove running ID
+      QDEL:{
+	  # Delete all other jobs
+	  print "--- qdel $ids ---\n";
+	  `$qdel $ids`;
+	  # Check if the delete succeeded
+	  sleep 2;
+	  @results = `$qstat | grep $pattern`;
+	  $ids = "";
+	  foreach (@results){
+	      /^(\d+).*([A-Z]) +(\S+)/;
+	      print "id=$1: $_";
+	      $ids .= " $1" unless $1 eq $running;
+	  }
+	  # Delete jobs again if there are any jobs left
+	  redo QDEL if $ids;
+	}
 	last LOOP;
     }
-
     sleep 5;
     redo;
 }
 
-print `qstat -u \$USER`;
+print `$qstat`;
 
 print "Finished watch, job $running is running\n";
