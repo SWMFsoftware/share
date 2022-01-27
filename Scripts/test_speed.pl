be90b2196b3f29e9a9d8f06e310005c2cd38b0ad
Gabor  Toth <gtoth@csrsrv2021-0006vm1.engin.umich.edu>
2022-01-27 14:22:53 -0500

    Initial version for a script to extract run times for 1 or more tests from one or more swmf_test.log files. The output is easy to read, but it could possibly by visualized in the future.

---
+++ Scripts/test_speed.pl
@@ -0,0 +1,62 @@
+#!/usr/bin/perl -s
+
+# Calculate run speed for various tests.
+
+my $Tests = $t;
+
+use strict;
+
+if(not @ARGV){
+    print "
+Get the run time of various tests from multiple test_swmf.log files.
+Can be used to check how speed changes over time.
+
+Usage:
+   test_speed [-t=TESTS] DIR1 [DIR2 ...]
+
+-t=TESTS      list of tests for which timings are required. By default all tests are evaluated.
+
+Example:
+   test_speed -t=earth,awsom_gpu SWMF_TEST_RESULTS/2022/01/2[0-2]/pleiades/
+";
+    exit 0;
+}
+
+my %Test;
+my %Speed;
+my $test;
+my $file;
+my $dir;
+
+foreach $dir (@ARGV){
+    $file = "$dir/test_swmf.log";
+    if(not open(FILE, $file)){
+	warn "$file is missing\n";
+	next;
+    }
+    
+    while(<FILE>){
+	if(/make test_(\w+)_run\b/){
+	    $test = $1;
+	    if($Tests){
+		next unless $Tests =~ /\b$test\b/;
+	    }
+	    # Found running the test
+	    $Test{$test}++;
+	}
+	# Extract the run time from a line like
+	# "PostProc.pl: TIMINGS from runlog (init, run) 1.81 1.05"
+	$Speed{$dir}{$test} = $1 if /TIMINGS/ and /([\d\.]+)$/;
+
+	$Speed{$dir}{$test} = $2 if
+	    /.*(BATSRUS|SWMF)\s+(\d+\.\d+).*/ or
+	    /.*(BATSRUS|SWMF)\s*1\s*1\s+(.*?)\s+/;
+    }
+}
+
+foreach $test (sort keys %Test){
+    foreach $dir (@ARGV){
+	printf "%s %20s: %4.2f\n", $dir, $test, $Speed{$dir}{$test};
+    }
+}
+
