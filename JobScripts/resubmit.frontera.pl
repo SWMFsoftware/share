d42f7b0c119e91c1845a783287c26414b233a6b0
Toth <gtoth@mesh.engin.umich.edu>
2022-02-25 14:59:49 -0500

    Generalized somewhat.

--- JobScripts/resubmit.frontera.pl
+++ JobScripts/resubmit.frontera.pl
@@ -1,22 +1,32 @@
-#!/usr/bin/perl
-# Usage: 
-# Edit this file to set things and then run it as
-#
-# resubmit.frontera.pl >& resubmit.log &
-#
+#!/usr/bin/perl -s
+
+my $jobfile = $ARGV[0];
+if(not $jobfile or $h or $help){
+    print "
+Usage: resubmit.frontera.pl jobfile >& resubmit.log & 
+";
+    exit 0;
+}
+use strict;
 
 my $Success1 = "SWMF.SUCCESS";
 my $Success2 = "BATSRUS.SUCCESS";
+my $Done1    = "SWMF.DONE";
+my $Done2    = "BATSRUS.DONE";
 
-while(not -d "RESTART_t0003.0000h"){      # final restart file
-    unlink $Success1, $Success2;          # remove success indicator
-    'Restart.pl';                         # process restart files
-    `sbatch job.frontera`;                # submit job
-    print "job submitted on ", `date`;
-    sleep 7000;                           # no need to check yet
-    sleep 5 while not (-f $Success1 or -f $Success2); 
+unlink $Done1, $Done2;                    # remove stop indicators
+while(not -f $Done1 and not -f $Done2){   # loop until done
+    unlink $Success1, $Success2;          # remove success indicators
+    `sbatch $jobfile`;                    # submit job
+    print "$jobfile submitted on ", `date`;
+    sleep 10 while not (-f $Success1 or -f $Success2); 
     print "job finished successfully on ", `date`;
     sleep 60;                             # wait for the job to quit
+    'Restart.pl';                         # process restart files
+    if(-f "PARAM.in.restart" and not -f "PARAM.in.start"){
+	`mv PARAM.in PARAM.in.start`;
+	`cp PARAM.in.restart PARAM.in`;
+    }
 }
 print "job reached final time on ", `date`;
 exit 0;
