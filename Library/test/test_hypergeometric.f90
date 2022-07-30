927e3fcfd7136f80baae8cc0dd5ccdae43692f49
Igor V. Sokolov <igorsok@umich.edu>
2022-07-29 21:23:55 -0400

    Improve comments

--- Library/test/test_hypergeometric.f90
+++ Library/test/test_hypergeometric.f90
@@ -7,8 +7,8 @@ program test
   use ModPlotFile
   implicit none
   integer, parameter :: nStep = 400, SCR_ = 1, L0Ext_=2, Approx_ = 3
-  integer, parameter :: AzimuthalS_ = 1, PoloidalS_ = 2, &
-       AzimuthalU_ = 3, PoloidalU_ = 4, ToroidalU_ = 5
+  integer, parameter :: AxialS_ = 1, PoloidalS_ = 2, &
+       AxialU_ = 3, PoloidalU_ = 4, ToroidalU_ = 5
   real   , parameter :: DeltaKappaPrime = 0.0010
   integer            :: iLoop
   real               :: R0, a, Q0, Q1, CurrentE,         &
@@ -16,6 +16,7 @@ program test
        KappaPrime0, KappaPrime02, Kappa0, Kappa02, Kappa03
   real :: Var_VI(SCR_:ToroidalU_,nStep), Coord_I(nSTep)
   !--------------------------
+  !!!!!!!!!!!!!!!!!!!!!! Plot inductances !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
   do iLoop = 1, nStep
      KappaPrime0  = iLoop*DeltaKappaPrime
      KappaPrime02 =  KappaPrime0**2 
@@ -33,35 +34,40 @@ program test
        StringFormatIn = '(4es18.10)'          ,&
        Coord1In_I = Coord_I                   ,&
        VarIn_VI = Var_VI(SCR_:Approx_,1:nStep) )
-  ! test Green function (field from the current over the toroid surface
+  !!!!!!!!!!!!!!!!!!!!! Inner field !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  ! Parameters on the plasma filament surface at which \kappa^\prime_0 = 0.1
   KappaPrime0 = 0.10 
   KappaPrime02 = KappaPrime0**2
   Kappa02 = 1 - KappaPrime02 
   Kappa0 = sqrt(Kappa02); Kappa03 = Kappa0*Kappa02
-  ! Eq. (26), constant field factor for surface current
+  ! Inner field from the current over the toroid surface
+  ! Eq. (31), constant field factor for surface current
   Q0 = 0.125*toroid_p(0, KappaPrime2In=KappaPrime02)/&
        toroid_q(0,KappaPrime2In=KappaPrime02)
   do iLoop = 1, nStep/2
+     ! \kappa^\prime ranges from 0 to \kappa^\prime_0 = 0.1
      KappaPrime = iLoop*0.50*DeltaKappaPrime
      KappaPrime2 = KappaPrime**2
      Kappa2 = 1 - KappaPrime2; Kappa = sqrt(Kappa2); Kappa3 = Kappa*Kappa2
      Coord_I(iLoop) = KappaPrime
      ! Eq. 26 for surface curent
-     Var_VI(AzimuthalS_,iLoop) = Q0*Kappa3*toroid_q(0,&
+     Var_VI(AxialS_,iLoop) = Q0*Kappa3*toroid_q(0,&
           KappaPrime2In=KappaPrime2)
-     Var_VI(PoloidalS_,iLoop) = 3*KappaPrime*Q0*Kappa3* toroid_q(1&
-          &,KappaPrime2In=KappaPrime2)
+     Var_VI(PoloidalS_,iLoop) = 3*KappaPrime*Q0*Kappa3*toroid_q(1,&
+          KappaPrime2In=KappaPrime2)
   end do
-  ! Eqs. 29
+  ! Eq. 36, constant field factor for uniform current
   Q1 = 0.125*toroid_p(1,KappaPrime2In=KappaPrime02)/&
        (toroid_q(1,KappaPrime2In=KappaPrime02))
+  ! Constant current I_E
   CurrentE = -1/(3*KappaPrime02*Kappa0*toroid_q(1,KappaPrime2In=KappaPrime02))
+  ! Inner field from the uniform current
   do iLoop = 1, nStep/2
      KappaPrime = iLoop*0.50*DeltaKappaPrime
      KappaPrime2 = KappaPrime**2
      Kappa2 = 1 - KappaPrime2; Kappa = sqrt(Kappa2); Kappa3 = Kappa*Kappa2
-     ! Eqs. 28
-     Var_VI(AzimuthalU_,iLoop) = Q1*Kappa3*toroid_q(0,KappaPrime2In = &
+     ! Eqs. 35
+     Var_VI(AxialU_,iLoop) = Q1*Kappa3*toroid_q(0,KappaPrime2In = &
           KappaPrime2) + CurrentE
      Var_VI(PoloidalU_,iLoop) = 3*Q1*KappaPrime*Kappa3*toroid_q(1,    &
           KappaPrime2In=KappaPrime2)
@@ -71,15 +77,16 @@ program test
           *(Kappa03*toroid_q(0,KappaPrime2In=KappaPrime02)     &
           - Kappa3*toroid_q(0,KappaPrime2In=KappaPrime2) )     )
   end do
+  !  External magnetic field 
   do iLoop = 1 + nStep/2, nStep
      KappaPrime = iLoop*0.50*DeltaKappaPrime
      KappaPrime2 = KappaPrime**2
      Kappa2 = 1 - KappaPrime2; Kappa = sqrt(Kappa2); Kappa3 = Kappa*Kappa2
      Coord_I(iLoop) = KappaPrime
-     ! Eqs. 24
-     Var_VI(AzimuthalS_,iLoop) = 0.125*Kappa3*toroid_p(0, KappaPrime2In = &
+     ! Eqs. 27
+     Var_VI(AxialS_,iLoop) = 0.125*Kappa3*toroid_p(0, KappaPrime2In = &
           KappaPrime2)
-     Var_VI(AzimuthalU_,iLoop) = 0.125*Kappa3*toroid_p(0, KappaPrime2In = &
+     Var_VI(AxialU_,iLoop) = 0.125*Kappa3*toroid_p(0, KappaPrime2In = &
           KappaPrime2)
      Var_VI(PoloidalS_,iLoop) = 0.375*Kappa3*KappaPrime* &
           toroid_p(1, KappaPrime2In = KappaPrime2)
@@ -90,9 +97,9 @@ program test
   call save_plot_file(NameFile='test_fields.out', &
        TypeFileIn='ascii'                     ,&
        NameVarIn=&
-       'kappa_prime AzimuthalS PoloidalS AzimuthalU_ PoloidalU ToroidalU', &
+       'kappa_prime AxialS PoloidalS AxialU PoloidalU ToroidalU', &
        StringFormatIn = '(6es18.10)'          ,&
        Coord1In_I = Coord_I                   ,&
-       VarIn_VI = Var_VI(AzimuthalS_:ToroidalU_,1:nStep))
+       VarIn_VI = Var_VI(AxialS_:ToroidalU_,1:nStep))
 end program test
 
