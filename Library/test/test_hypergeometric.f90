b8d0c28a230eef9234856290f86f6cdde588ea35
Igor V. Sokolov <igorsok@umich.edu>
2022-08-18 23:09:36 -0400

    Modify plot with the inductances

--- Library/test/test_hypergeometric.f90
+++ Library/test/test_hypergeometric.f90
@@ -7,33 +7,33 @@ program test
   use ModCurrentFilament
   use ModFieldGS, ONLY: test_gs22=>test
   implicit none
-  integer, parameter :: nStep = 400, SCR_ = 1, L0Ext_=2, Approx_ = 3
+  integer, parameter :: nStep = 400, SCR_ = 3, L0Ext_=1, Approx_ = 2
   integer, parameter :: AxialS_ = 1, PoloidalS_ = 2, &
        AxialU_ = 3, PoloidalU_ = 4, ToroidalU_ = 5
   real   , parameter :: DeltaKappaPrime = 0.0010
   integer            :: iLoop
   real               :: R0, a, Amplitude_I(Axial_:Toroidal_), &
        KappaPrime, KappaPrime2, Kappa2, Kappa3
-  real :: Var_VI(SCR_:ToroidalU_,nStep), Coord_I(nSTep)
+  real :: Var_VI(L0Ext_:ToroidalU_,nStep), Coord_I(nSTep)
   !----------------------------------------------------------------------------
   !!!!!!!!!!!!!!!!!!!!!! Plot inductances !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
   do iLoop = 1, nStep
-     KappaPrime  = iLoop*DeltaKappaPrime
+     KappaPrime  = iLoop*DeltaKappaPrime*0.5
      KappaPrime2 =  KappaPrime**2
      a  = 2*KappaPrime/(1 - KappaPrime2)
      R0 = (1 + KappaPrime2)/(1 - KappaPrime2)
      Coord_I(iLoop) = a/R0
      Var_VI(SCR_,iLoop)   = scr_inductance(KappaPrime2)
      Var_VI(L0Ext_,iLoop) = l0_ext_inductance(KappaPrime2)
-     Var_VI(Approx_,iLoop) = log(8.0/Coord_I(iLoop)) - 2.0
+     Var_VI(Approx_,iLoop) = R0*log(8.0/Coord_I(iLoop)) - 2.0
   end do
   call save_plot_file(NameFile='test_inductance.out', &
        TypeFileIn='ascii'                     ,&
        NameVarIn = &
-       'a_over_R0 Inductance_SCR L_Ext_inductance Approximate_inductance', &
+       'a_over_R0 L_Ext_inductance Approximate_inductance Inductance_SCR', &
        StringFormatIn = '(4es18.10)'          ,&
        Coord1In_I = Coord_I                   ,&
-       VarIn_VI = Var_VI(SCR_:Approx_,1:nStep) )
+       VarIn_VI = Var_VI(L0Ext_:SCR_,1:nStep) )
   !!!!!!!!!!!!!!!!!!!!!  Internal field  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
   ! Parameters on the plasma filament surface at which \kappa^\prime_0 = 0.1
   UseUniformCurrent = .true.;  UseSurfaceCurrent = .true.
