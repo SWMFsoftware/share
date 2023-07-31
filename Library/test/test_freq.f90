6c3b3be09b8c2859f2c4b344869a007d454a4815
Igor V. Sokolov <igorsok@umich.edu>
2023-07-31 16:45:33 -0400

    Unintentionally formatted

--- Library/test/test_freq.f90
+++ Library/test/test_freq.f90
@@ -1,4 +1,5 @@
-!  Copyright (C) 2002 Regents of the University of Michigan, portions used with permission 
+!  Copyright (C) 2002 Regents of the University of Michigan,
+!  portions used with permission
 !  For more information, see http://csem.engin.umich.edu/tools/swmf
 program test_freq
 
@@ -6,7 +7,9 @@ program test_freq
 
   implicit none
 
+  !----------------------------------------------------------------------------
   call test
 
 end program test_freq
+!==============================================================================
 
