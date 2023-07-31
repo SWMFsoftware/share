43ed4099e719f1f84496d1b5e6d8fceeb2274381
Igor V. Sokolov <igorsok@umich.edu>
2023-07-31 16:48:39 -0400

    Unintentionally formatted

--- Library/test/test_lookup_table.f90
+++ Library/test/test_lookup_table.f90
@@ -1,4 +1,5 @@
-!  Copyright (C) 2002 Regents of the University of Michigan, portions used with permission 
+!  Copyright (C) 2002 Regents of the University of Michigan,
+!  portions used with permission
 !  For more information, see http://csem.engin.umich.edu/tools/swmf
 program test_lookup_table
 
@@ -8,12 +9,11 @@ program test_lookup_table
   implicit none
 
   integer :: iError
-  !--------------------------------------------------------------------------
+  !----------------------------------------------------------------------------
   call MPI_init(iError)
   call test
   call MPI_finalize(iError)
 
 end program test_lookup_table
-
-!=============================================================================
+!==============================================================================
 
