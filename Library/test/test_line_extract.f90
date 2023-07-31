2ff81015c06fa609877b54d957372e2ebc9a7046
Igor V. Sokolov <igorsok@umich.edu>
2023-07-31 16:43:43 -0400

    Unintentionally formatted

--- Library/test/test_line_extract.f90
+++ Library/test/test_line_extract.f90
@@ -1,4 +1,5 @@
-!  Copyright (C) 2002 Regents of the University of Michigan, portions used with permission 
+!  Copyright (C) 2002 Regents of the University of Michigan,
+!  portions used with permission
 !  For more information, see http://csem.engin.umich.edu/tools/swmf
 program test_line
 
@@ -8,10 +9,11 @@ program test_line
 
   integer :: iError
 
+  !----------------------------------------------------------------------------
   call MPI_init(iError)
   call line_test
   call MPI_finalize(iError)
 
 end program test_line
-
+!==============================================================================
 
