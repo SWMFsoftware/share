22102a6b02755b72434ff0368c2d7c9cdf36441b
Gabor Toth <gtoth@csrwks2022-0212.engin.umich.edu>
2025-03-11 13:58:59 -0400

    Improvements. Get dims from coordinate array shape. Not tested for 1D data etc. Not tested with params. Still has issues with string endings.

--- Python/Scripts/swmf_plotfile.py
+++ Python/Scripts/swmf_plotfile.py
@@ -38,22 +38,22 @@ def file_format(filename):
         return "real8"
     else:
         return "ascii"
-    print('bytes=', bytes, 'reclen=', reclen)
-    return "ascii"
     
 ###############################################################################
 def read_plotfile(filename, fileformat='unknown'):
+    # read the file "filename" and return a dictionary with the content
+    # use fileformat if set
     if fileformat == "unknown":
-        # figure out the file format
         fileformat = file_format(filename)
 
     if fileformat == "real4" or fileformat == "real8":
-        return read_binary(filename, fileformat)
+        return read_binary(filename)
     else:
         return read_ascii(filename)
 
 ###############################################################################
 def write_plotfile(data, filename="plotfile.out", fileformat="18.10e"):
+    # write data into the SWMF file filename with format fileformat
     if fileformat == 'real4' or fileformat == 'real8':
         write_binary(data, filename, fileformat)
     else:
@@ -61,10 +61,11 @@ def write_plotfile(data, filename="plotfile.out", fileformat="18.10e"):
     return
 
 ###############################################################################
-def read_ascii(filename):
-    # read ASCII SWMF file "filename" and return a dictionary with the content
+def read_ascii(filename, verbose=False):
+    # read ASCII SWMF file filename and return a dictionary with the content
+
     f = open(filename,'r')
-    head = f.readline()
+    head = f.readline()[:-1]
     step, time, ndim, npar, nvar = f.readline().split()
     step = np.int32(step)
     time = np.float64(time)
@@ -74,9 +75,9 @@ def read_ascii(filename):
     dims = np.array(f.readline().split(), dtype=np.int32)
     if npar > 0:
         pars = np.array(f.readline().split(), dtype=np.float64)
-    name = f.readline()
-    ngrid = np.prod(dims)
+    name = f.readline()[:-1]
     # read the remaining numbers (this will not work for .outs files)
+    ngrid = np.prod(dims)
     griddata = np.array(f.read().split(), dtype=np.float64)
     # reshape and transpose array to match default Python ordering
     griddata = griddata.reshape(ngrid, ndim+nvar).T
@@ -85,16 +86,16 @@ def read_ascii(filename):
     coord = griddata[:ndim,:]
     state = griddata[ndim:,:]
 
-    # debug
-    #print('head=',head)
-    #print('step,time,ndim,npar,nvar=', step, time, ndim, npar, nvar)
-    #print('dims=',dims)
-    #print('ngrid=',ngrid)
-    #if npar > 0:
-    #    print('pars=', pars)
-    #print('name=', name)
-    #print('coord=',coord)
-    #print('state=',state)
+    if verbose:
+        print('filename=', filename)
+        print('head=',head)
+        print('step,time,ndim,npar,nvar=', step, time, ndim, npar, nvar)
+        print('dims=',dims, 'ngrid=', ngrid)
+        if npar > 0:
+            print('pars=', pars)
+        print('name=', name)
+        print('coord.shape=',coord.shape)
+        print('state.shape=',state.shape)
 
     # Create dictionary
     data ={"head"   : head,
@@ -111,23 +112,23 @@ def read_ascii(filename):
         data["pars"] = pars 
     return data
 ###############################################################################
-def read_binary(filename, fileformat='unknown'):
+def read_binary(filename, verbose=False):
     # read binary SWMF file "filename" and return a dictionary with the content
-
-    if fileformat == "unknown":
-        fileformat = file_format(filename)
-
-    if fileformat == "real4":
-        nreal = 4
-        dtype = np.float32
-    else:
-        nreal = 8
-        dtype = np.float64
 
     f = open(filename,'rb')
     stringlength = int.from_bytes(f.read(4),'little')
-    head = f.read(stringlength).decode()
-    f.read(8) # skip markers
+    head = f.read(stringlength).decode().rstrip()
+    f.read(4) # skip markers
+    len2 = int.from_bytes(f.read(4),'little')
+    if len2 == 20:
+        # single precision reals
+        nreal = 4
+        dtype = np.float32
+    else:
+        # double precision reals
+        nreal = 8
+        dtype = np.float64
+        
     step = int.from_bytes(f.read(4),'little')
     time = np.frombuffer(f.read(nreal), dtype=dtype)[0]
     ndim = int.from_bytes(f.read(4),'little')
@@ -139,7 +140,7 @@ def read_binary(filename, fileformat='unknown'):
     if npar > 0:
         pars = np.frombuffer(f.read(nreal*npar), dtype=dtype)
         f.read(8) # skip markers
-    name = f.read(stringlength).decode()
+    name = f.read(stringlength).decode().rstrip()
     f.read(8) # skip markers
     ngrid = np.prod(dims)
     coord = np.frombuffer(f.read(nreal*ngrid*ndim), dtype=dtype).reshape(ndim, ngrid)
@@ -149,13 +150,16 @@ def read_binary(filename, fileformat='unknown'):
         state[i,:] = np.frombuffer(f.read(nreal*ngrid), dtype=dtype)
     f.close
 
-    #print("step, time, ndim, npar, nvar=", step, time, ndim, npar, nvar)
-    #print("dims=", dims)
-    #if npar > 0:
-    #    print("pars=", pars)
-    #print("name=", name)
-    #print("coord=", coord)
-    #print("state=", state)
+    if verbose:
+        print('filename=', filename)
+        print('head=',head)
+        print('step,time,ndim,npar,nvar=', step, time, ndim, npar, nvar)
+        print('dims=',dims, 'ngrid=', ngrid)
+        if npar > 0:
+            print('pars=', pars)
+        print('name=', name)
+        print('coord.shape=',coord.shape)
+        print('state.shape=',state.shape)
 
     # Create dictionary
     data ={"head"   : head,
@@ -176,8 +180,8 @@ def read_binary(filename, fileformat='unknown'):
 def fortran_string(string, l):
     # create bytestring of length l with spaces added at the end
     if len(string) > l:
-        print("ERROR in Fortran string:")
-        print("Length of string=",len(string),"> l=",l)
+        print("ERROR in fortran_string:")
+        print("Length of string=", len(string),"> l=",l)
         exit(1)
     return string.encode()[:-1] + b' '*(l+1 - len(string))
 ###############################################################################
@@ -185,6 +189,15 @@ def fortran_record(bytearray):
     # write a Fortran record with 4-byte markers at both ends
     reclen = np.int32(len(bytearray)).tobytes()
     return reclen + bytearray + reclen
+###############################################################################
+def get_dims(coord):
+    # get the grid dimensions from the coordinate array
+    dims = list(coord.shape)
+    if coord.ndim > 1:
+        dims = dims[1:] # remove the first index
+        dims.reverse()  # reverse the order (Fortran)
+    return dims
+    
 ###############################################################################
 def write_ascii(data, filename="plotfile.out", fileformat="18.10e"):
     # Print data into ascii file filename using fileformat for real arrays.
@@ -200,7 +213,7 @@ def write_ascii(data, filename="plotfile.out", fileformat="18.10e"):
     coord = np.float64(data["coord"])
     state = np.float64(data["state"])
     head = data["head"] if "head" in data else "Missing head"
-    dims = data["dims"] if "dims" in data else coord.shape[1:]
+    dims = np.int32(data["dims"] if "dims" in data else get_dims(coord))
     step = data["step"] if "step" in data else 0
     time = data["time"] if "time" in data else 0.0
     pars = np.float64(data["pars"]) if "pars" in data else []
@@ -222,19 +235,19 @@ def write_ascii(data, filename="plotfile.out", fileformat="18.10e"):
         print("name=", name," npar=", npar)
         exit(1)
 
+    coord = coord.reshape(ndim,ngrid)
+    state = state.reshape(nvar,ngrid)
+        
     float_formatter = "{:"+fileformat+"}"
-
-    print("float_formatter =",float_formatter)
     np.set_printoptions(formatter={'float_kind':float_formatter.format})
-    f.writelines(head)
+    f.writelines(head+"\n")
     template = "{0:d} {1:"+fileformat+"} {2:d} {3:d} {4:d}\n"
     f.write(template.format(step, time, ndim, npar, nvar))
     f.write(str(dims)[1:-1]+"\n")
     template = "{:18.10e}"
     if npar > 0:
         f.write(str(pars)[1:-1]+"\n")
-    f.writelines(name)
-    print("coord.shape=", coord.shape)
+    f.writelines(name+"\n")
     for i in range(ngrid):
         f.write(str(coord[:,i])[1:-1] + str(state[:,i])[1:-1] + "\n")
 
@@ -263,25 +276,25 @@ def write_binary(data, filename="plotfile.out", fileformat="real4"):
 
     # convert to real4 / real8 as needed
     if fileformat == "real8":
-        if coord.dtype != "float64":
+        if not isinstance(coord, np.ndarray) or coord.dtype != "float64":
             coord = np.float64(coord)
-        if state.dtype != "float64":
+        if not isinstance(state, np.ndarray) or state.dtype != "float64":
             state = np.float64(state)
         time = np.float64(time)
-        if npar > 0 and pars.dtype != "float64":
+        if npar > 0:
             pars = np.float64(pars)
             
     else:
-        if coord.dtype != "float32":
+        if not isinstance(coord, np.ndarray) or coord.dtype != "float32":
             coord = np.float32(coord)
-        if state.dtype != "float32":
+        if not isinstance(state, np.ndarray) or state.dtype != "float32":
             state = np.float32(state)
         time = np.float32(time)
         if npar > 0 and pars.dtype != "float32":
             pars = np.float32(pars)
             
     head = data["head"] if "head" in data else "Missing head"
-    dims = np.int32(data["dims"]) if "dims" in data else coord.shape[1:]
+    dims = np.int32(data["dims"] if "dims" in data else get_dims(coord))
     step = np.int32(data["step"] if "step" in data else 0)
 
     ndim = np.int32(len(dims))
@@ -319,26 +332,65 @@ def write_binary(data, filename="plotfile.out", fileformat="real4"):
 
 ###############################################################################
 def test_plotfile():
-    filein  = 'file_ascii.ini'
+
+    # minimal data
+    data = {
+        "name": "longitude latitude dBN dBE",
+        "coord": [[[1.,2.,3.,4.],[1.,2.,3.,4.],[1.,2.,3.,4.]],
+                  [[1.,1.,1.,1.],[2.,2.,2.,2.],[3.,3.,3.,3.]]],
+        "state": [[[10.,11.,12.,13.],[100.,110.,120.,130.],[1000.,1100.,1200.,1300]],
+                  [[20.,21.,22.,23.],[200.,210.,220.,230.],[2000.,2100.,2200.,2300]]]
+        }
+
+    print("name=", data["name"])
+    print("coord.shape=", data["coord"])
+    print("state.shape=", data["state"])
+
+    # write ASCII file with 3 decimals
+    fileout = 'file_ascii.out'
+    write_plotfile(data, fileout, '10.3f')
+    print("Type of "+fileout+":", file_format(fileout))
+    
+    # write single precision binary file
     fileout = 'file_real4.out'
-    print("Type of "+filein+":", file_format(filein))
-    print('reading   ', filein)
-    data = read_plotfile(filein)
-    write_plotfile(data, fileout, "real4")
+    write_plotfile(data, fileout, "real8")
     print('wrote out ', fileout)
     print("Type of "+fileout+":", file_format(fileout))
 
-    print('reading  ', fileout)
-    data2 = read_plotfile(fileout)
+    # Add head line and dimensions
+    data["head"] = "Proper header"
+    data["dims"] = [12, 1]
 
+    # write double precision binary file
     fileout = 'file_real8.out'
     write_plotfile(data, fileout, "real8")
+    print('wrote out ', fileout)
     print("Type of "+fileout+":", file_format(fileout))
 
-    fileout = 'file_ascii.out'
-    write_plotfile(data2, fileout, '10.3f')
-    print("Type of "+fileout+":", file_format(fileout))
+    # read back ascii file
+    filein = 'file_ascii.out'
+    print('reading  ', filein)
+    data2 = read_plotfile(filein)
+    print("head=",data2["head"])
+    print("dims=",data2["dims"])
+    print("name=",data2["name"])
 
+    # read back single precision binary file
+    filein = 'file_real4.out'
+    print('reading  ', filein)
+    data2 = read_plotfile(filein)
+    print("head=",data2["head"])
+    print("dims=",data2["dims"])
+    print("name=",data2["name"])
+
+    # read back double precision binary file
+    filein = 'file_real8.out'
+    print('reading  ', filein)
+    data2 = read_plotfile(filein)
+    print("head=",data2["head"])
+    print("dims=",data2["dims"])
+    print("name=",data2["name"])
+    
     return
 ###############################################################################
 test_plotfile()
