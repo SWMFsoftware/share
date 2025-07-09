d934d6fc3ea94f3ff029507d5b4798c21e4ca70d
Gabor Toth <gtoth@umich.edu>
2025-07-09 16:15:34 -0400

    An attempt to get AMReX compile on derecho. Seems to work with the 2023 compiler suite. Might be work with the newer compilers, but derecho died, so cannot test it.

--- Scripts/Config.pl
+++ Scripts/Config.pl
@@ -932,7 +932,7 @@ sub set_amrex_{
 	$AmrexCompiler="pgi" if $Compiler eq "pgf90" or $Compiler eq "nvfortran";       
 
 	&shell_command("cd util/AMREX;", 
-		       "./configure --prefix $InstallDir --comp $AmrexCompiler --enable-fortran-api no --debug $Debug --enable-tiny-profile yes --dim $NewAmrexDim;",
+		       "./configure --prefix $InstallDir --comp $AmrexCompiler --enable-fortran-api no --debug $Debug --enable-tiny-profile yes --dim $NewAmrexDim --allow-different-compiler yes;",
 		       "rm -rf util/AMREX/$InstallDir");
 	
 	$IsStrict = 0;
