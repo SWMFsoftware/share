0ce48ccccd193847c03d82c9a84a0fee6948f1c6
Gabor Toth <gtoth@umich.edu>
2026-08-18 16:24:51 -0400

    Simplified $Dir matching

--- Scripts/PostProc.pl
+++ Scripts/PostProc.pl
@@ -148,7 +148,7 @@ REPEAT:{
             &concat_sat_log if $Concat;
 	}elsif( $Dir =~ /^PC|PT$/ ){
 	    &shell($pIDL);
-	}elsif( $Dir =~ "UA"){
+	}elsif( $Dir eq "UA"){
 	    if (-e "./post_process.py") {
 		&shell("./pGITM -np $nThread");
 	    }else{
@@ -168,31 +168,31 @@ REPEAT:{
 		}
 	    }
             &concat_sat_log if $Concat;
-	}elsif( $Dir =~ /^IM/ ){
+	}elsif( $Dir eq "IM" ){
 	    my @files=glob("plots/*.dat");
 	    if($Gzip){
 		&shell("gzip",@files) if @files;
 	    }else{
 		&shell("./Preplot.pl",@files) if @files;
 	    }
-	}elsif( $Dir =~ /^PS/ ){
+	}elsif( $Dir eq "PS" ){
 	    my @files=glob("Output/dgcpm*.dat");
 	    if($Gzip){
 		&shell("gzip", @files) if @files;
 	    }
-	}elsif( $Dir =~ /^PW/ ){
+	}elsif( $Dir eq "PW" ){
 	    # PWOM output files cannot be gzipped while code is running
 	    # because it is appending to the files.
 	    if($Gzip and not $Repeat){
 		my @files=glob("plots/*.out");
 		&shell("gzip", @files) if @files;
 	    }
-	}elsif( $Dir =~ /^RB/ ){
+	}elsif( $Dir eq "RB" ){
 	    if($Gzip){
 		my @files=glob("plots/*.fls");
 		&shell("gzip",@files) if @files;
 	    }
-	}elsif( $Dir =~ /^plots$/ ){
+	}elsif( $Dir eq "plots" ){
 	    &shell("cd ..; $pIDL");
 	}
 	chdir $Pwd;
